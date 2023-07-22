09d362390546256ac2642dcc2665bad1a2cd14ec
Fesdem <festusdemiladeoni@gmail.com>
2023-07-22 14:10:30 +0100

    Added more lines of code to the main.dart file

--- main.dart
+++ main.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 void main() {
   for (int i = 0; i < 2; i++) {
     print('hello ${i + 1}');
@@ -9,4 +11,34 @@ void main() {
       if (i != 5) i
   ];
   print(numbers);
+  for (int i = 1; i <= 10; i++) {
+    print(i);
+  }
+
+  int rand = Random().nextInt(10);
+
+  if (rand < 5) {
+    print("smaller 5");
+  } else {
+    print("greater 5");
+  }
+
+  switch (rand) {
+    case 0:
+      print("null");
+      break;
+
+    case 1:
+      print("one");
+      continue onefour;
+    case 2:
+      print("two");
+      break;
+    onefour:
+    case 4:
+      print("One or four");
+      break;
+    default:
+      print("none");
+  }
 }
